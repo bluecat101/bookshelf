cacfcadb6b43c6c7d53e5ad915290441b930e678
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2025-04-07 18:16:33 +0900

    topページの作成

---
+++ lib/index.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Index extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('index'),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
---
+++ lib/newBook.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class NewBook extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('newBook'),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
