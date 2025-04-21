3beb3d2dad7e5a9bd630775f096c35e9c6703f1b
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2025-04-21 22:48:37 +0900

    sizeの取得

--- lib/apis/national_diet_library_api.dart
+++ lib/apis/national_diet_library_api.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:xml/xml.dart';
 import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+
+const bookSize = {
+  15: {"width": 8},
+  19: {"width": 13},
+  21: {"width": 15},
+  23: {"width": 15},
+  24: {"width": 16},
+  30: {"width": 21},
+};
 
 class NdlBook {
   final String title;
@@ -37,15 +47,12 @@ List<NdlBook> parseNdlBooks(String xmlString, String searchedTitle) {
     }
 
     final identifiers = item.findElements('dc:identifier');
-    debugPrint('identifiers: $identifiers');
     String? imageUrl;
 
     for (final id in identifiers) {
       final typeAttr = id.getAttribute('xsi:type');
-      debugPrint(typeAttr);
       if (typeAttr == 'dcndl:ISBN') {
         final isbnStr = id.innerText;
-        debugPrint('isbn: $isbnStr');
         final isbn = int.tryParse(isbnStr.replaceAll('-', ''));
         imageUrl = 'https://ndlsearch.ndl.go.jp/thumbnail/$isbn.jpg';
         break;
@@ -60,6 +67,28 @@ List<NdlBook> parseNdlBooks(String xmlString, String searchedTitle) {
   return ndlBooks;
 }
 
+({int? width, int? height, int? page}) parseNdlBookSize(String htmlString) {
+  final document = parse(htmlString);
+  // 大きさ等が書いてある
+  final elements = document.getElementsByClassName(
+    'base-layout-column pages-books-meta-panel',
+  );
+  final infoItems = elements[0].getElementsByClassName('base-layout-row');
+  final text = infoItems[6].getElementsByTagName("span")[0].text;
+  final regex = RegExp(r'(\d+)p\s*;\s*(\d+)cm');
+  final match = regex.firstMatch(text.toString());
+  if (match != null) {
+    final page = int.parse(match.group(1)!);
+    final height = int.parse(match.group(2)!);
+    if (bookSize[height] == null) {
+      return (width: null, height: height, page: page);
+    }
+    final width = bookSize[height]?['width'];
+    return (width: width, height: height, page: page);
+  }
+  return (width: null, height: null, page: null);
+}
+
 Future<List<NdlBook>> fetchBookInfoThroughNationalDietLibrary(
   String title,
 ) async {
@@ -69,6 +98,18 @@ Future<List<NdlBook>> fetchBookInfoThroughNationalDietLibrary(
   final response = await http.get(Uri.parse(url));
   if (response.statusCode == 200) {
     return parseNdlBooks(response.body, title);
+  } else {
+    throw Exception('Failed to fetch data');
+  }
+}
+
+Future<({int? width, int? height, int? page})> fetchBookSize(
+  NdlBook ndlBook,
+) async {
+  final response = await http.get(Uri.parse(ndlBook.link));
+  if (response.statusCode == 200) {
+    final size = parseNdlBookSize(response.body);
+    return (width: size.height!, height: size.width!, page: size.page!);
   } else {
     throw Exception('Failed to fetch data');
   }
