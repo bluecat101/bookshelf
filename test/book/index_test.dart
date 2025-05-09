ef6241718a65b24e7d76619b9bc70079263359ec
bluecat101 <97800922+bluecat101@users.noreply.github.com>
2025-05-10 06:56:41 +0900

    index_testをDIに適応(一部)

--- test/book/index_test.dart
+++ test/book/index_test.dart
@@ -1,31 +1,56 @@
+import 'package:bookshelf/helper/url.dart';
+import 'package:bookshelf/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bookshelf/book/index.dart';
 import 'package:bookshelf/book/show.dart';
 import 'package:bookshelf/book/model/book.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'index_test.mocks.dart';
 
-// ダミーデータの作成
-Future<List<Book>> dummyBooks() {
-  final books = [
-    Book(
-      title: 'sample title1',
-      author: 'sample author1',
-      pages: 1,
-      height: 1,
-      width: 1,
-      coverImageUrl: 'https://picsum.photos/200/300',
-    ),
-    Book(
-      title: 'sample title1',
-      author: 'sample author1',
-      pages: 2,
-      height: 2,
-      width: 2,
-      comment: 'sample comment',
-      coverImageUrl: 'https://picsum.photos/200/300',
-    ),
+late MockUrlHelperImpl mockUrlHelperImpl;
+
+Book makeDummyBook({
+  String title = 'sample title',
+  String author = 'sample author',
+  int pages = 1,
+  int height = 1,
+  int width = 1,
+  String? coverImageUrl,
+  String? coverImagePath,
+  String? spineImagePath,
+}) {
+  return Book(
+    title: title,
+    author: author,
+    pages: pages,
+    height: height,
+    width: width,
+    coverImageUrl: coverImageUrl,
+    coverImagePath: coverImagePath,
+    spineImagePath: spineImagePath,
+  );
+}
+
+Future<void> mockExistUrl({required bool result}) async {
+  when(mockUrlHelperImpl.existUrl(any)).thenAnswer((_) async => result);
+}
+
+Future<List<Book>> dummyBooks() async {
+  return [
+    // URLのみ
+    makeDummyBook(coverImageUrl: 'https://picsum.photos/200/300'),
+
+    // 画像のpathを持っている
+    // makeDummyBook(
+    //   coverImagePath: '/assets/sample.png',
+    //   spineImagePath: '/assets/spine_sample.png',
+    // ),
+
+    // // 画像なし
+    // makeDummyBook(),
   ];
-  return Future.value(books);
 }
 
 Future<void> displayDialog(WidgetTester tester) async {
@@ -35,7 +60,64 @@ Future<void> displayDialog(WidgetTester tester) async {
   await tester.pumpAndSettle();
 }
 
+void initDI() {
+  setUpAll(() async {
+    mockUrlHelperImpl = MockUrlHelperImpl();
+    getIt.registerLazySingleton<UrlHelperImpl>(() => mockUrlHelperImpl);
+  });
+
+  tearDownAll(() async {
+    getIt.reset();
+  });
+}
+
+@GenerateMocks([UrlHelperImpl])
 void main() {
+  initDI();
+  testWidgets('[成功時]表紙の画像が登録されている時はその画像が表示される', (WidgetTester tester) async {
+    mockExistUrl(result: true);
+    await tester.pumpWidget(
+      MaterialApp(home: Index(booksFuture: dummyBooks())),
+    );
+    await tester.pumpAndSettle();
+    final targetBook = find.byType(SizedBox).at(0);
+    await tester.tap(targetBook);
+    await tester.pumpAndSettle();
+
+    // final imageFinder = find.descendant(
+    //   of: find.byType(Dialog),
+    //   matching: find.byType(Image),
+    // );
+    // final imageWidget = tester.widget<Image>(imageFinder); // ← ここでWidgetに変換
+
+    // final provider = imageWidget.image as NetworkImage;
+    // expect(provider.url, equals('https://example.com/image.png'));
+  });
+
+  // testWidgets(
+  //   '[成功時]表紙の画像が登録されていない時は画像URLの画像が表示される',
+  //   (WidgetTester tester) async {},
+  // );
+  // testWidgets(
+  //   '[成功時]表紙の画像が登録されておらず、画像URLがない場合は画像が表示されずタイトルが表示される',
+  //   (WidgetTester tester) async {},
+  // );
+
+  // testWidgets(
+  //   '[成功時]背表紙の画像が登録されておらず、画像URLがない場合は画像が表示されずタイトルが表示される',
+  //   (WidgetTester tester) async {},
+  // );
+
+  // testWidgets(
+  //   '[成功時]背表紙の画像が登録されておらず、画像URLがない場合は画像が表示されずタイトルが表示される',
+  //   (WidgetTester tester) async {},
+  // );
+
+  // testWidgets(
+  //   '[成功時]背表紙の画像が登録されておらず、画像URLがない場合は画像が表示されずタイトルが表示される',
+  //   (WidgetTester tester) async {},
+  // );
+
   testWidgets('表示されている個数が合っている', (WidgetTester tester) async {
     await tester.pumpWidget(
       MaterialApp(home: Index(booksFuture: dummyBooks())),
